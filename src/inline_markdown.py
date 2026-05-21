3f2db014f3c56645082c2fd96f834391d4749a96
RinGke <keaganspires@gmail.com>
2026-05-21 19:36:26 +0000

    added split_nodes_images and split_nodes_links with test

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -1,32 +1,80 @@
 import re
 
-from src.textnode import TextNode, TextType
+from src.textnode import *
 
 
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
     new_nodes = []
     for o in old_nodes:
-        if o.text_type == "image" or o.text_type == "link":
+        if o.text_type != TextType.TEXT:
             new_nodes.append(o)
-        elif delimiter not in o.text:
-            new_nodes.append(o)
-        else:
-            splits = o.text.split(delimiter)
-            if len(splits) < 3 or not len(splits) % 2:
-                raise Exception("Invalid markdown syntax: missing closing delimiter!")
-            for i, s in enumerate(splits):
-                if i % 2:
-                    new_nodes.append(TextNode(text=s, text_type=text_type))
-                else:
-                    new_nodes.append(TextNode(text=s, text_type=TextType.TEXT))
+            continue
+        splits = o.text.split(delimiter)
+        if not len(splits) % 2:
+            raise Exception("Invalid markdown syntax: missing closing delimiter!")
+        for i, s in enumerate(splits):
+            if s == "":
+                continue
+            if i % 2:
+                new_nodes.append(TextNode(text=s, text_type=text_type))
+            else:
+                new_nodes.append(TextNode(text=s, text_type=TextType.TEXT))
     return new_nodes
 
 
 def extract_markdown_images(text):
-    matches = re.findall(r"!\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
-    return matches
+    return re.findall(r"!\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
 
 
 def extract_markdown_links(text):
-    matches = re.findall(r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
-    return matches
+    return re.findall(r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
+
+
+def split_nodes_images(old_nodes):
+    new_nodes = []
+    for o in old_nodes:
+        if o.text_type != TextType.TEXT:
+            new_nodes.append(o)
+            continue
+        matches = extract_markdown_images(o.text)
+        if not matches:
+            new_nodes.append(o)
+            continue
+        splits = re.split(r"(!\[.*?\]\(.*?\))", o.text)
+        for s in splits:
+            if s == "":
+                continue
+            for m in matches:
+                if s == f"![{m[0]}]({m[1]})":
+                    new_nodes.append(
+                        TextNode(text=m[0], text_type=TextType.IMAGE, url=m[1])
+                    )
+                    break
+            else:
+                new_nodes.append(TextNode(text=s, text_type=TextType.TEXT))
+    return new_nodes
+
+
+def split_nodes_links(old_nodes):
+    new_nodes = []
+    for o in old_nodes:
+        if o.text_type != TextType.TEXT:
+            new_nodes.append(o)
+            continue
+        matches = extract_markdown_links(o.text)
+        if not matches:
+            new_nodes.append(o)
+            continue
+        splits = re.split(r"(?<!!)(\[.*?\]\(.*?\))", o.text)
+        for s in splits:
+            if s == "":
+                continue
+            for m in matches:
+                if s == f"[{m[0]}]({m[1]})":
+                    new_nodes.append(
+                        TextNode(text=m[0], text_type=TextType.LINK, url=m[1])
+                    )
+                    break
+            else:
+                new_nodes.append(TextNode(text=s, text_type=TextType.TEXT))
+    return new_nodes
--- src/tests/test_inline_markdown.py
+++ src/tests/test_inline_markdown.py
@@ -106,3 +106,57 @@ class TestInlineMarkdown(unittest.TestCase):
             ],
             matches,
         )
+
+    def test_split_images(self):
+        node = TextNode(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png) and another ![second image](https://i.imgur.com/3elNhQu.png)",
+            TextType.TEXT,
+        )
+        node2 = TextNode(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png) and a link [to boot dev](https://www.boot.dev)",
+            TextType.TEXT,
+        )
+        new_nodes = split_nodes_images([node, node2])
+        self.assertListEqual(
+            [
+                TextNode("This is text with an ", TextType.TEXT),
+                TextNode("image", TextType.IMAGE, "https://i.imgur.com/zjjcJKZ.png"),
+                TextNode(" and another ", TextType.TEXT),
+                TextNode(
+                    "second image", TextType.IMAGE, "https://i.imgur.com/3elNhQu.png"
+                ),
+                TextNode("This is text with an ", TextType.TEXT),
+                TextNode("image", TextType.IMAGE, "https://i.imgur.com/zjjcJKZ.png"),
+                TextNode(
+                    " and a link [to boot dev](https://www.boot.dev)",
+                    TextType.TEXT,
+                ),
+            ],
+            new_nodes,
+        )
+
+    def test_split_links(self):
+        node = TextNode(
+            "This is text with a [to youtube](https://www.youtube.com) and another [to boot dev](https://www.boot.dev)",
+            TextType.TEXT,
+        )
+        node2 = TextNode(
+            "This is text with a [to youtube](https://www.youtube.com) and an image ![image](https://i.imgur.com/zjjcJKZ.png)",
+            TextType.TEXT,
+        )
+        new_nodes = split_nodes_links([node, node2])
+        self.assertListEqual(
+            [
+                TextNode("This is text with a ", TextType.TEXT),
+                TextNode("to youtube", TextType.LINK, "https://www.youtube.com"),
+                TextNode(" and another ", TextType.TEXT),
+                TextNode("to boot dev", TextType.LINK, "https://www.boot.dev"),
+                TextNode("This is text with a ", TextType.TEXT),
+                TextNode("to youtube", TextType.LINK, "https://www.youtube.com"),
+                TextNode(
+                    " and an image ![image](https://i.imgur.com/zjjcJKZ.png)",
+                    TextType.TEXT,
+                ),
+            ],
+            new_nodes,
+        )
